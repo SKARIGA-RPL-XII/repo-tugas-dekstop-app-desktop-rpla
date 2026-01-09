5793d915731c18878775a7dffc18a2d3e8fbbef2
DevCode.Id <154581685+MuhammadZaki07@users.noreply.github.com>
2026-01-09 13:59:07 +0700

    refactor code

--- applications/src/server/routes/category.js
+++ applications/src/server/routes/category.js
@@ -3,18 +3,7 @@ import { categoryController } from "../controllers/categories.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("Category route: /");
-});
-
-router.get("/getAllCategories", async (req, res) => {
-    try {
-        categoryController.getCategories(req, res);
-    } catch (error) {
-        res.status(500).send({ message: "Internal Server Error" });
-    }
-});
-
+router.get("/getAllCategories", categoryController.getCategories);
 router.post("/createCategories", categoryController.createCategory);
 router.get("/getCategoryById/:id", categoryController.getCategoryById);
 router.put("/updateCategories/:id", categoryController.updateCategory);
