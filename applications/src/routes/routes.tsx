471364d9e9edd1f54a52a78aca4cbe194886a2e7
Hasya Rayyan  Bahaudin Mahardika <hasyarayyanbm@gmail.com>
2026-01-12 20:14:50 +0700

    Update routes.tsx

--- applications/src/routes/routes.tsx
+++ applications/src/routes/routes.tsx
@@ -2,13 +2,21 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../pages/layouts/mainLayout";
 import Login from "../pages/login";
 import NotFound from "../pages/errors/NotFound";
-import Pengguna from "../pages/pengguna";
-import Kategori from "../pages/kategori";
+
 import Dashboard from "../pages/dashboard";
+import Kategori from "../pages/kategori";
+
+import Produk from "../pages/produk";
+import ProdukTambah from "../pages/produk/tambah";
+import ProdukEdit from "../pages/produk/edit";
+import ProdukDetail from "../pages/produk/detail";
+
+import Pengguna from "../pages/pengguna";
+import PenggunaDetail from "../pages/pengguna/detail";
 
 const router = createBrowserRouter([
   {
-    path: "admin",
+    path: "/admin",
     element: <MainLayout />,
     children: [
       {
@@ -20,28 +28,60 @@ const router = createBrowserRouter([
         element: <Dashboard />,
       },
       {
-        path: "Kategori",
+        path: "kategori",
         element: <Kategori />,
       },
+
+      // ===== PENGGUNA =====
       {
         path: "pengguna",
-        element: <Pengguna />,
+        children: [
+          {
+            index: true,
+            element: <Pengguna />,              // /admin/pengguna
+          },
+          {
+            path: "detail/:id",
+            element: <PenggunaDetail />,        // /admin/pengguna/detail/1
+          },
+        ],
+      },
+
+      // ===== PRODUK =====
+      {
+        path: "produk",
+        children: [
+          {
+            index: true,
+            element: <Produk />,               // /admin/produk
+          },
+          {
+            path: "tambah",
+            element: <ProdukTambah />,         // /admin/produk/tambah
+          },
+          {
+            path: "edit/:id",
+            element: <ProdukEdit />,           // /admin/produk/edit/1
+          },
+          {
+            path: "detail/:id",
+            element: <ProdukDetail />,         // /admin/produk/detail/1
+          },
+        ],
       },
     ],
   },
+
   {
     path: "/login",
     element: <Login />,
   },
-  {
-    path: "/employe",
-    element: <MainLayout />,
-    children: [],
-  },
+
   {
     path: "/",
     element: <Login />,
   },
+
   {
     path: "*",
     element: <NotFound />,
