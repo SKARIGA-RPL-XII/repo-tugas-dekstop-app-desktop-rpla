97c5d71d6a427c51b7c44f86303c7b92ce302b66
DevCode.Id <154581685+MuhammadZaki07@users.noreply.github.com>
2026-01-18 17:28:16 +0700

    update schema + validation create user

--- applications/src/server/schemas/user.schema.js
+++ applications/src/server/schemas/user.schema.js
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const createUserSchema = z.object({
   username: z.string().min(3, "username minimal 3 karakter"),
+  name: z.string().min(3, "name minimal 3 karakter"),
   email: z.string().email("email tidak valid"),
   password: z.string().min(6, "password minimal 6 karakter"),
 });
